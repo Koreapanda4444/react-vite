ffec4f5676a9ac41e712471ebdff7283f84667c2
koreapanda4444 <jun20091019@gmail.com>
2025-08-23 12:20:34 +0900

    커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from './Layout';
 import Home from "./Home";
 import About from "./About";
 import Flag from "./Flag";
+import RacingGame from "./RacingGame";
+import Lotto from "./Lotto";
+import SelfIntro from "./SelfIntro";
+import Floating from "./Floating";
+import Pacman from "./Pacman";
+import Univ from "./Univ";
+import Tetris from "./Tetris";
 
 function App() {
   
@@ -14,8 +22,15 @@ function App() {
             <Route index element={<Home />} />
             <Route path="About" element={<About />} />
             <Route path="Flag" element={<Flag />} />
+            <Route path="racing" element={<RacingGame />} />
+            <Route path="lotto" element={<Lotto />} />
+            <Route path="intro" element={<SelfIntro />} />
+            <Route path="pacman" element={<Pacman />} />
+            <Route path="Univ" element={<Univ />} />
+            <Route path="tetris" element={<Tetris />} />
           </Route>
         </Routes>
+        <Floating />
       </BrowserRouter>
     </>
   )
